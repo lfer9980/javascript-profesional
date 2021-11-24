43e2a858b2566d5561ae5f45c6fe4c9d2ad51c8b
lfer9980 <luis_fernandez99@hotmail.com>
2021-11-24 00:57:17 -0700

    clase 9: this

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,9 @@ import MediaPlayer from './mediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins:[new AutoPlay() ]});
+const player = new MediaPlayer({ el: video, plugins:[
+	//new AutoPlay() 
+]});
 
 const playButton = document.getElementById('button-play');
 const muteButton = document.getElementById('button-mute')
