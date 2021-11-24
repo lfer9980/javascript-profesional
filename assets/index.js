85656e8533d800f8d670ccc28cbea2ef0c2ac5f4
lfer9980 <luis_fernandez99@hotmail.com>
2021-11-24 00:03:49 -0700

    clase 6: clousures

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,8 @@
-class MediaPlayer {
-	constructor(config) {
-		this.media = config.el;
-	}
-	play() {
-		this.media.play()
-	}
-	pause() {
-		this.media.pause()
-	}
-	togglePlay() {
-		this.media.paused
-			? this.play()
-			: this.pause()
-	}
-}
+import MediaPlayer from './mediaPlayer'
+
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({ el: video });
 
 const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+button.onclick = () => player.togglePlay();	
